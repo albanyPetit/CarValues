55bd05fbf9a9b0ca49b2482900c7eb8f0fd16973
albanyPetit <albanydaniellepetit@gmail.com>
2025-07-03 20:50:01 +0200

    create user method

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common'; //para validar datos de una request
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //detalle de seguridad. elimina los atributos extras ingresados por el usuario en el request y que no son los especificados
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    //usamos el decoretor, llamamos al arg body y definimos su tipo como DTO
+    //console.log(body);
+    this.usersService.create(body.email, body.password);
+  }
+}
