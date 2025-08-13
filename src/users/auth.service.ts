6e6feb7a4c1edf5528b2c044ddff7bbccf7ef716
albanyPetit <albanydaniellepetit@gmail.com>
2025-08-13 16:34:26 +0200

    sign-in n sign-out process

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,5 +1,13 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt } from 'crypto'; //el primero para generar la salt y el segundo es la funcion hash
+import { promisify } from 'util';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
 export class AuthService {
@@ -13,11 +21,37 @@ export class AuthService {
     }
 
     //hash the users password
+    //Generate a salt
+    const salt = randomBytes(8).toString('hex');
+
+    //Hash the salt and the password together
+    const hash = (await scrypt(password, salt, 32)) as Buffer; //ultimo parametro e sla longitud
+
+    //Join the hashed result and the salt together -> this is what we put in our DB
+    const result = salt + '.' + hash.toString('hex');
 
     //create a new user and save it
+    const user = await this.usersService.create(email, result);
 
     //rerurn the user if everything is ok
+    return user;
   }
 
-  signin() {}
+  async signin(email: string, password: string) {
+    //see if email is in use
+    const [user] = await this.usersService.find(email);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const [salt, storedHash] = user.password.split('.');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('Bad password');
+    }
+
+    return user;
+  }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,23 +8,47 @@ import {
   Query,
   Body,
   NotFoundException,
+  Session,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
+
+  @Get('/whoami')
+  whoAmI(@Session() session: any) {
+    return this.usersService.findOne(session.userId);
+  }
+
+  @Post('/signout')
+  signOut(@Session() session: any) {
+    session.userId = null;
+  }
 
   @Post('/signup')
-  createUser(@Body() body: CreateUserDto) {
+  async createUser(@Body() body: CreateUserDto, @Session() session: any) {
     //usamos el decoretor, llamamos al arg body y definimos su tipo como DTO
     //console.log(body);
-    this.usersService.create(body.email, body.password);
+    const user = await this.authService.signup(body.email, body.password);
+    session.userId = user.id;
+    return user;
+  }
+
+  @Post('/signin')
+  async signin(@Body() body: CreateUserDto, @Session() session: any) {
+    const user = await this.authService.signin(body.email, body.password);
+    session.userId = user.id;
+    return user;
   }
 
   //@UseInterceptors(new SerializeInterceptor(UserDto))
