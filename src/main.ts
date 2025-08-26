2b52c92ec75d358ff75ec1b1534ecdfb0cfe2966
albanyPetit <albanydaniellepetit@gmail.com>
2025-08-26 19:31:37 +0200

    e2e testing done

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common'; //para validar datos de una request
-
-const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['asdfasfd'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, //detalle de seguridad. elimina los atributos extras ingresados por el usuario en el request y que no son los especificados
-    }),
-  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
